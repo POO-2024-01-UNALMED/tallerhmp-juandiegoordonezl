491011f41946f58a9e385b9e516a10d27fbbe86d
juandiegoordonezl <159456074+juandiegoordonezl@users.noreply.github.com>
2024-08-12 22:19:00 -0500

    Reimaginacion 5

--- persona.py
+++ persona.py
@@ -1,32 +1,30 @@
-class Persona:
-    def __init__(self, nombre, edad, altura, sexo):
-        self._nombre = nombre
-        self._edad = edad
-        self._altura = altura
-        self._sexo = sexo
-
-    # Métodos get
-    def get_nombre(self):
+class Persona():
+    def __init__(self,nombre,edad,altura,sexo):
+        self._nombre=nombre
+        self._edad=edad
+        self._altura=altura 
+        self._sexo=sexo 
+    
+    def getNombre(self):
         return self._nombre
-
-    def get_edad(self):
+    
+    def setNombre(self,nombre):
+        self._nombre=nombre 
+    
+    def getEdad(self):
         return self._edad
-
-    def get_altura(self):
-        return self._altura
-
-    def get_sexo(self):
+    
+    def setEdad(self,edad):
+        self._edad=edad
+        
+    def getAltura(self):
+        return self._altura 
+    
+    def setAltura(self,altura):
+        self._altura=altura 
+    
+    def getSexo(self):
         return self._sexo
-
-    # Métodos set
-    def set_nombre(self, nombre):
-        self._nombre = nombre
-
-    def set_edad(self, edad):
-        self._edad = edad
-
-    def set_altura(self, altura):
-        self._altura = altura
-
-    def set_sexo(self, sexo):
-        self._sexo = sexo
+    
+    def setSexo(self,sexo):
+        self._sexo=sexo 
